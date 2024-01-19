0459460e3005460385f69a0d3a12f69eb88d9419
Wendesday36 <bertazoeanna@gmail.com>
2024-01-19 14:44:43 +0100

    css

--- alapsablon/src/pages/Csapat.js
+++ alapsablon/src/pages/Csapat.js
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function Csapat() {
     function myFunction(x) {
         x.classList.toggle("change");
       }
   return (
-    <div class="container" onclick={myFunction(this)}>
-      <div class="bar1"></div>
-      <div class="bar2"></div>
-      <div class="bar3"></div>
+    <div className="csapatcontainer" onClick={myFunction(this)}>
+      <div className="bar1"><a href=""></a></div>
+      <div className="bar2"><a href=""></a></div>
+      <div className="bar3"><a href=""></a></div>
     </div>
   );
 }
