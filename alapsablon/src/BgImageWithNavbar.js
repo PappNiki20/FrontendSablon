2e9eb1b0640063f98605f08623ee6f432ca62f42
PappNiki20 <papp.nikoletta001@gmail.com>
2024-01-21 15:25:53 +0100

    fooldal elrendezese, hiro headerrel

---
+++ alapsablon/src/BgImageWithNavbar.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const BgImageWithNavbar = () => {
+  return (
+    <header style={{ paddingLeft: 0 }}>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"  
+            src="kepek/bg2.jpg"
+            alt="Slide 1"
+          />
+          <div className="carousel-caption d-none d-md-block">
+            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', height: '100vh' }}>
+              <div className='d-flex justify-content-center align-items-center h-100'>
+                <div className='text-white text-center'>
+                  <h1 className='mb-3'>ARC Project</h1>
+                  <h3 className='mb-3'>Üdvözöllek az oldalon!</h3>
+                  <h4 className='mb-4'>
+                    {/* Lorem Ipsum szöveg */}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </h4>
+                  <Link className='btn btn-outline-light btn-lg' to='/csapat' role='button'>
+                    Csapatok
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+          className="d-block w-100"
+            src="kepek/bg.jpg"
+            alt="Slide 2"
+          />
+          <div className="carousel-caption d-none d-md-block">
+            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', height: '100vh' }}>
+              <div className='d-flex justify-content-center align-items-center h-100'>
+                <div className='text-white text-center'>
+                  <h1 className='mb-3'>ARC Project</h1>
+                  <h3 className='mb-3'>Üdvözöllek az oldalon!</h3>
+                  <h4 className='mb-4'>
+                    {/* Lorem Ipsum szöveg */}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </h4>
+                  <Link className='btn btn-outline-light btn-lg' to='/csapat' role='button'>
+                    Csapatok
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+          className="d-block w-100"
+            src="kepek/bg3.jpg"
+            alt="Slide 3"
+          />
+          <div className="carousel-caption d-none d-md-block">
+            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', height: '100vh' }}>
+              <div className='d-flex justify-content-center align-items-center h-100'>
+                <div className='text-white text-center'>
+                  <h1 className='mb-3'>ARC Project</h1>
+                  <h3 className='mb-3'>Üdvözöllek az oldalon!</h3>
+                  <h4 className='mb-4'>
+                    {/* Lorem Ipsum szöveg */}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </h4>
+                  <Link className='btn btn-outline-light btn-lg' to='/csapat' role='button'>
+                    Csapatok
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </header>
+    
+  );
+}
+
+export default BgImageWithNavbar;
--- alapsablon/src/pages/Fooldal.js
+++ alapsablon/src/pages/Fooldal.js
@@ -1,6 +1,67 @@
-import '../css/Fooldal.css';
-export default function Fooldal(){
-    return(
-        <div className="fooldal"> fooldal  </div>
-    )
-}
+// Fooldal.js
+import "../css/Fooldal.css";
+import React from 'react';
+import BgImageWithNavbar from '../BgImageWithNavbar';
+
+const Fooldal = () => {
+  return (
+    <>
+      <BgImageWithNavbar />
+    <div>
+      <div className="summary-section">
+        <div className="summary-content">
+          <div className="summary-text">
+       <h1>Csapatok</h1>
+
+            <p>
+             
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <div className="summary-image">
+           
+            <img src="kepek/bg2.jpg" alt="Kép leírása" style={{ maxWidth: '600px', width: '100%' }}/>
+          </div>
+        </div>
+
+        <span></span>
+
+
+        <div className="summary-content left">
+        <div className="summary-text left">
+     <h1>Alkotók</h1>
+
+          <p>
+           
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="summary-image left">
+         
+          <img src="kepek/bg3.jpg" alt="Kép leírása" style={{ maxWidth: '600px', width: '100%' }}/>
+        </div>
+      </div>
+      
+    <span></span>
+      
+      <div className="summary-content">
+      <div className="summary-text">
+   <h1>Projektek</h1>
+
+        <p>
+         
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div className="summary-image">
+       
+        <img src="kepek/bg.jpg" alt="Kép leírása" style={{ maxWidth: '600px', width: '100%' }}/>
+      </div>
+    </div>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default Fooldal;
