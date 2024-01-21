60fb0d3c141df16de094c0e2a796f62f35202b15
PappNiki20 <papp.nikoletta001@gmail.com>
2024-01-21 13:34:52 +0100

    navbar szin beallitasa

--- alapsablon/src/LayOut.js
+++ alapsablon/src/LayOut.js
@@ -17,25 +17,27 @@ const LayOut = () => {
     <>
       <Navbar>
         <Container fluid>
-          <Button className='ms-auto' type='button' onClick={toggleNavbar}>
+          <Button className='ms-auto bg-opacity-50' type='button' variant='secondary' onClick={toggleNavbar}>
             <FontAwesomeIcon icon={faBars} />
           </Button>
         </Container>
       </Navbar>
 
       <Collapse in={isOpen}>
-        <div className='bg-light shadow-3 p-4 nav'>
-          <Button as={Link} to="/" variant='link' block className='border-bottom m-0'>
-            Főoldal
-          </Button>
-          <Button as={Link} to="alkoto" variant='link' block className='border-bottom m-0'>
-            Alkotó
-          </Button>
-          <Button as={Link} to="csapat" variant='link' block className='m-0'>
-            Csapat
-          </Button>
-        </div>
-      </Collapse>
+  <div className=' shadow-3 p-4 nav' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)'}} >
+    <Button as={Link} to="/" variant='link' block className='text-black m-0'>
+      Főoldal
+    </Button>
+    <Button as={Link} to="alkoto" variant='link' block className='text-black m-0'>
+      Alkotó
+    </Button>
+    <Button as={Link} to="csapat" variant='link' block className='text-black m-0'>
+      Csapat
+    </Button>
+  </div>
+</Collapse>
+
+    
 
       {/* Ide kerül majd az útvonalak/linkek által meghatározott tartalom */}
       <Outlet />
