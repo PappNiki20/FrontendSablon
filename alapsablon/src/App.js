3de1b9df27f96d80348017353ebfb1d9c3a7e8e2
PappNiki20 <papp.nikoletta001@gmail.com>
2024-01-21 13:02:56 +0100

    hamburger menu kialakitasa

--- alapsablon/src/App.js
+++ alapsablon/src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
-//import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './LayOut';
 import Fooldal from './pages/Fooldal';
 import Alkoto from './pages/Alkoto';
 import Csapat from './pages/Csapat';
-import Layout from './LayOut';
-
 
 function App() {
   return (
-    <div>
-      <header><h1>Arc Projekt</h1> </header>
-      
-      <BrowserRouter>
+    <div className='menu'>
+      <header><h1>Arc Projekt</h1></header>
+    <div className='feljebb'>
+      <BrowserRouter >
+     
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Fooldal />} />
@@ -21,6 +20,8 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      </div>
+      
     </div>
   );
 }
--- alapsablon/src/LayOut.js
+++ alapsablon/src/LayOut.js
@@ -1,30 +1,46 @@
-import { Outlet, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Navbar, Container, Button, Collapse } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Link, Outlet } from 'react-router-dom';
 
-const Layout = () => {
+const LayOut = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            {/* A publikus tartalom linkje */}
-            <Link to="/">Főoldal</Link>
-          </li>
-          <li>
-            {/* Az admin oldal linkjetartalom linkje */}
-            <Link to="/alkoto">Alkotó</Link>
-          </li>
-          <li>
-            {/* Az admin oldal linkjetartalom linkje */}
-            <Link to="/csapat">Csapat</Link>
-          </li>
-        </ul>
-      </nav>
-      <article>
-        {/* Ide kerül majd az útvonalak/linkek által meghatározott tartalom */}
-        <Outlet />
-      </article>
+      <Navbar>
+        <Container fluid>
+          <Button className='ms-auto' type='button' onClick={toggleNavbar}>
+            <FontAwesomeIcon icon={faBars} />
+          </Button>
+        </Container>
+      </Navbar>
+
+      <Collapse in={isOpen}>
+        <div className='bg-light shadow-3 p-4 nav'>
+          <Button as={Link} to="/" variant='link' block className='border-bottom m-0'>
+            Főoldal
+          </Button>
+          <Button as={Link} to="alkoto" variant='link' block className='border-bottom m-0'>
+            Alkotó
+          </Button>
+          <Button as={Link} to="csapat" variant='link' block className='m-0'>
+            Csapat
+          </Button>
+        </div>
+      </Collapse>
+
+      {/* Ide kerül majd az útvonalak/linkek által meghatározott tartalom */}
+      <Outlet />
     </>
   );
 };
 
-export default Layout;
+export default LayOut;
