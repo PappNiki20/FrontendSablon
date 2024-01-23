7982a2509843b824e55e7d9340cd2d99a1741089
Wendesday36 <bertazoeanna@gmail.com>
2024-01-23 13:17:32 +0100

    buszekesegek

--- alapsablon/src/pages/Buszkesegeink.js
+++ alapsablon/src/pages/Buszkesegeink.js
@@ -1,18 +1,32 @@
 import { alkotok } from "../Alkotok";
-export default function Buszkesegeink(){
-    return(
-        <div className="buszkesegek">
-        {/* {alkotok.map((elem, index) => {
-        <div className="kep" >
-        <h2>{elem.nev}</h2>
-            <img src={props.obj.src} alt=""/>
-        <div className="container">
-        <p>{props.obj.leiras}</p>
-        <p>{props.obj.ar}</p>
-        <button type="button" onClick={kattintas}>Kosárba</button>
-        </div>
-        </div>})} */}
-        </div>
-    );
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+export default function Buszkesegeink() {
+  const AlkotoCard = ({ nev, src, szak }) => (
+    <Card style={{ width: '200px' }}>
+    <Card.Img variant="top" src={src} alt="" />
+    <Card.Body>
+      <Card.Title>{nev}</Card.Title>
+      <Card.Text>{szak}</Card.Text>
+      <Link to={`/alkoto`}>
+        <Button variant="primary">Részletek</Button>
+      </Link>
+    </Card.Body>
+  </Card>
+  );
+  return (
+    <div className="buszkesegek ">
+      {alkotok.map((elem, index) => (
+        <AlkotoCard
+          key={index}
+          nev={elem.nev}
+          src={elem.src}
+          szak={elem.szak}
+          kattintas={() => console.log("katt")}
+        />
+      ))}
+    </div>
+  );
 }
