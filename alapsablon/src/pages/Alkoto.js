6a3188f1f5415aad79b7a37070514523af008a98
PappNiki20 <papp.nikoletta001@gmail.com>
2024-01-27 21:48:02 +0100

    reszponzivitas elkezdese, alkoto terv

--- alapsablon/src/pages/Alkoto.js
+++ alapsablon/src/pages/Alkoto.js
@@ -1,120 +1,154 @@
-import "../css/Kozos.css";
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Alkoto() {
   return (
-    <div className="alkoto summary-section " style={{ padding: "20px" }}>
+    <div className="summary-section container">
       <div className="cont">
-        <header className="text-center">
-          <h1>Zöldlomb Legolas</h1>
-        </header>
-       
-          <div >
-            <h2
-              style={{
-                fontSize: "1.5em",
-                fontWeight: "bold",
-                marginBottom: "10px",
-                textAlign: "justify",
-              }}
-            >
-              Szak:
-            </h2>
-            <p
-              style={{
-                fontSize: "1.2em",
-                marginBottom: "10px",
-                textAlign: "justify",
-              }}
-            >
-              Grafikus
-            </p>
-            <h2
-              style={{
-                fontSize: "1.5em",
-                fontWeight: "bold",
-                marginBottom: "10px",
-                textAlign: "justify",
-              }}
-            >
-              {" "}
-              Projekt neve:
-            </h2>
-            <p
-              style={{
-                fontSize: "1.2em",
-                marginBottom: "10px",
-                textAlign: "justify",
-              }}
-            >
-              Újrahasznosított fólia-projekt
-            </p>
-            <h2
-              style={{
-                fontSize: "1.5em",
-                fontWeight: "bold",
-                marginBottom: "10px",
-                textAlign: "justify",
-              }}
-            >
-              {" "}
-              Kategória:
-            </h2>
-            <p
-              style={{
-                fontSize: "1.2em",
-                marginBottom: "10px",
-                textAlign: "justify",
-              }}
-            >
-              {" "}
-              Újrahasznosítás
-            </p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            padding: "20px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "flex-start",
+              marginBottom: "20px",
+              padding: "15px",
+              borderRadius: "10px",
+              width: "100%",
+            }}
+          >
+            <div style={{ display: "flex", flex: "1" }}>
+              <div style={{ marginRight: "20px", flex: "0 0 auto" }}>
+                <img
+                  src="./kepek/legolas.jpg"
+                  alt="a kep alt-ja ha nem tolt be"
+                  style={{
+                    maxWidth: "200px",
+                    height: "auto",
+                    borderRadius: "10%",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                  }}
+                  className="img-thumbnail"
+                />
+              </div>
+              <div style={{ flex: "1", display: "flex", flexDirection: "column" }}>
+                <div>
+                  <h1 style={{ fontSize: "2em", marginBottom: "10px" }}>
+                    Zöldlomb Legolas
+                  </h1>
+                  <p style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+                    Projekt neve: Újrahasznosított fólia-projekt
+                  </p>
+                  <p style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+                    Csapat neve: XYZ Team
+                  </p>
+                </div>
+                <Link to="/csapat">
+                  <button
+                    style={{
+                      marginTop: "1%",
+                      padding: "10px",
+                      fontSize: "1.2em",
+                      backgroundColor: "#3498db",
+                      color: "#fff",
+                      border: "none",
+                      borderRadius: "5px",
+                      cursor: "pointer",
+                      transition: "background-color 0.3s",
+                    }}
+                  >
+                    Csapat oldalra
+                  </button>
+                </Link>
+              </div>
             </div>
-            <div className="">
-              <img
-                src="./kepek/legolas.jpg"
-                alt="a kep alt-ja ha nem tolt be"
-                style={{
-                  maxWidth: "100%",
-                  height: "auto",
-                  borderRadius: "10%",
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-                }}
-                className="img-fluid"
-              />
-            
           </div>
-          <div className="col-md-6">
-            <h3>Bemutatkozás</h3>
+          <div
+            style={{
+              width: "100%",
+              borderBottom: "1px solid #ccc",
+              marginBottom: "20px",
+              paddingBottom: "20px",
+            }}
+          />
+
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(2, 1fr)",
+              gap: "20px",
+              marginBottom: "20px",
+              width: "100%",
+            }}
+          >
+            <div
+              style={{
+                textAlign: "center",
+                background: "rgb(248, 223, 188)",
+                padding: "20px",
+                borderRadius: "10px",
+              }}
+            >
+              <h2 style={{ fontSize: "1.5em", marginBottom: "10px" }}>Szak:</h2>
+              <p style={{ fontSize: "1.2em", marginBottom: "10px" }}>
+                Grafikus
+              </p>
+            </div>
+            <div
+              style={{
+                textAlign: "center",
+                background: "rgb(248, 223, 188)",
+                padding: "20px",
+                borderRadius: "10px",
+              }}
+            >
+              <h2 style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+                Kategória:
+              </h2>
+              <p style={{ fontSize: "1.2em", marginBottom: "10px" }}>
+                Újrahasznosítás
+              </p>
+            </div>
+          </div>
+
+          <div
+            style={{
+              padding: "20px",
+              width: "100%",
+            }}
+          >
+            <h3 style={{ fontSize: "2em", marginBottom: "10px" }}>
+              Bemutatkozás
+            </h3>
             <p
               style={{
                 textAlign: "justify",
-                fontSize: "1.1em",
+                fontSize: "1.5em",
                 lineHeight: "1.5",
               }}
             >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              quam lacus, vulputate ac porta ac, vehicula pharetra enim. In
-              ipsum felis, eleifend at erat in, volutpat condimentum dolor.
-              Vivamus ac orci lobortis nisl cursus sollicitudin eu a sem.
-              Integer tristique, lacus pellentesque fringilla porttitor, massa
-              tellus semper odio, ac ultrices felis purus vitae tortor.
-              Curabitur varius nisi arcu, at sollicitudin enim suscipit vitae.
-              In hac habitasse platea dictumst. Phasellus ac ligula finibus,
-              eleifend justo eget, dignissim risus. Aliquam sodales feugiat
-              pellentesque. Aliquam pharetra eget lorem ac eleifend. Maecenas et
-              purus eget erat convallis interdum. Proin placerat eros at arcu
-              molestie egestas. Fusce volutpat justo justo, id convallis est
-              sagittis at. Vestibulum ante ipsum primis in faucibus orci luctus
-              et ultrices posuere cubilia curae; Mauris accumsan sapien
-              porttitor convallis laoreet. Etiam rhoncus ligula ut lobortis
-              maximus. Duis suscipit tellus eu fermentum rhoncus. Curabitur arcu
-              nibh, cursus ac arcu egestas, suscipit pharetra arcu. Fusce id
-              massa id augue hendrerit luctus eu nec risus.
+              quam lacus, vulputate ac porta ac, vehicula pharetra enim.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam
+              lacus, vulputate ac porta ac, vehicula pharetra enim.Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Fusce quam lacus,
+              vulputate ac porta ac, vehicula pharetra enim.Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Fusce quam lacus, vulputate
+              ac porta ac, vehicula pharetra enim.Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Fusce quam lacus, vulputate ac porta
+              ac, vehicula pharetra enim.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Fusce quam lacus, vulputate ac porta ac, vehicula
+              pharetra enim.
             </p>
           </div>
         </div>
       </div>
-   
+    </div>
   );
 }
